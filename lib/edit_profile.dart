f2fdd99e5dc8b23dab1d23c88f59e27e15ebac2f
MengSeng99 <kohmengsengapu@gmail.com>
2024-10-09 15:11:16 +0800

    9/10

--- lib/edit_profile.dart
+++ lib/edit_profile.dart
@@ -6,7 +6,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 class EditProfileScreen extends StatefulWidget {
-  const EditProfileScreen({Key? key}) : super(key: key);
+  const EditProfileScreen({super.key});
 
   @override
   _EditProfileScreenState createState() => _EditProfileScreenState();
